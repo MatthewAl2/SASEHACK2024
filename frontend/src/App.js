4d013501d48ea3102bb91d8be1f2924d6063ef8b
alexanderlap <alex.lap118@gmail.com>
2024-10-04 22:37:43 -0400

    just updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import Home from './components/homepage';
 
 function App() {
   return (
+    <PrimeReactProvider>
         <Home />
+    </PrimeReactProvider>
   );
 }
 
