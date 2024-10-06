947ba7047de3249ed0ec4716c5653b487a4b5108
Karish Gupta <karishgupta2003@gmail.com>
2024-10-06 18:42:40 -0400

    sign up and login are conistent

--- frontend/src/pages/signUpPage.js
+++ frontend/src/pages/signUpPage.js
@@ -1,64 +1,104 @@
 import React, { useState, useEffect} from 'react';
 import Navbar from '../components/navbar';
-import ImageGallery from '../components/gallery';
 import { InputText } from 'primereact/inputtext';
 import axios from 'axios';
 import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
+import { Galleria } from 'primereact/galleria';
 
 export default function SignUp() {
-    // Define styles as JS objects
-    const styles = {
-        loginPage: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '90vh',
-            backgroundColor: '#f4f4f4',
-        },
-        loginContainer: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            padding: '20px',
-            backgroundColor: 'white',
-            borderRadius: '8px',
-            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-            width: '100%',
-            maxWidth: '640px',
-        },
-        galleryContainer: {
-            marginBottom: '20px',
-            width: '100%',
-            maxWidth: '640px',
-        },
-        loginForm: {
-            width: '100%',
-            maxWidth: '400px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        },
-        formGroup: {
-            width: '100%',
-            marginBottom: '15px',
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        label: {
-            fontSize: '14px',
-            marginBottom: '5px',
-            color: '#333',
-        },
-        inputText: {
-            width: '100%',
-            padding: '10px',
-            fontSize: '16px',
-            borderRadius: '4px',
-            border: '1px solid #ddd',
-        },
+    const galleria4 = '../images/galleria4.jpg';
+    const galleria5 = '../images/galleria5.jpg';    
+    const galleria6 = '../images/galleria6.jpg';
+    const galleria7 = '../images/galleria7.jpg';
+ 
+    const images = [
+       galleria4,
+       galleria5,
+       galleria6,
+       galleria7
+   ];
+ 
+   const itemTemplate = (item) => {
+       console.log(item)
+       console.log(item[1])
+       console.log(item[1].itemImageSrc)
+       return (
+          <img
+             src={item}
+             alt={item}
+             style={{
+             width: '100%',
+             height: '100%',
+             borderRadius: '3px'}}/>
+          );
     };
+ 
+ 
+    const thumbnailTemplate = (item) => {
+       return (
+           <img
+               src={item}
+               alt={item}
+               style={{
+                   height: '30px',
+                   borderRadius: '5px',
+               }}
+           />
+       );
+   };
+ 
+   const styles = {
+      loginPage: {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '90vh',
+          backgroundColor: '#f4f4f4',
+      },
+      loginContainer: {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '40px',
+          backgroundColor: 'white',
+          borderRadius: '8px',
+          boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+          width: '100%',
+          maxWidth: '640px',
+      },
+      galleryContainer: {
+          marginBottom: '20px',
+          width: '100%',
+          maxWidth: '100%',
+      },
+      loginForm: {
+          width: '100%',
+          maxWidth: '400px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+      },
+      formGroup: {
+          width: '100%',
+          marginBottom: '15px',
+          display: 'flex',
+          flexDirection: 'column',
+      },
+      label: {
+          fontSize: '14px',
+          marginBottom: '5px',
+          color: '#333',
+      },
+      inputText: {
+          width: '100%',
+          padding: '10px',
+          fontSize: '16px',
+          borderRadius: '4px',
+          border: '1px solid #ddd',
+      },
+  };
 
    const navigate = useNavigate();
    const [username , setUsername] = useState('');
@@ -121,7 +161,8 @@ export default function SignUp() {
          <div style={styles.loginPage}>
                <div style={styles.loginContainer}>
                   <div style={styles.galleryContainer}>
-                     <ImageGallery />
+                  <Galleria value={images} item={itemTemplate} thumbnail={thumbnailTemplate} numVisible={3} circular autoPlay transitionInterval={2000} showThumbnails={true} className ="custom-galleria" style={{ width: '400px', margin: '0 auto' }}/>
+
                   </div>
                   <div style={styles.loginForm}>
                      <div style={styles.formGroup}>
